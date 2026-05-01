e8b82acc04ce4f2305e18eb4a9fa135a876b3911
Mayank Choudhary <choudharymayank006@gmail.com>
2026-05-01 23:37:22 +0530

    Created secure signup route

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express'
+import { urlencoded } from 'express'
 
 const app = express()
 
 const PORT = process.env.PORT ?? 8000
+
+app.use(express.json())
+app.use(urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     return res.json({status: 'Server is up and running...'})
---
+++ routes/user.routes.js
@@ -0,0 +1,42 @@
+import express from 'express'
+
+import {db} from '../db/index.js'
+import {usersTable} from '../models/index.js'
+
+import {eq} from 'drizzle-orm'
+import { error } from 'node:console'
+
+import { randomBytes, createHmac } from 'crypto'
+
+
+const router = express.Router()
+
+router.post('/signup',  async(req, res) => {
+    const {firstname, lastname, email, password} = req.body
+    
+    const [existingUser] = await db
+    .select({
+        id: usersTable.id,
+    })
+    .from(usersTable)
+    .where(eq(usersTable.email, email))
+
+    if(existingUser) return res.status(400).json({error: `User with email ${email} already exists!`})
+
+    // Hash the Password
+    const salt = randomBytes(256).toString('hex')
+    const hashedPassword = createHmac('sha256', salt).update(password).digest('hex')
+
+    const [user] = await db.insert(usersTable).values({
+        email,
+        firstname,
+        lastname,
+        salt,
+        password: hashedPassword
+    }).returning({id: usersTable.id})
+
+    return res.status(201).json({data : {userId: user.id}})
+
+})
+
+export default router
