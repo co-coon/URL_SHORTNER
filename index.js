37ba664ab08a6e0d2c5c477e01969a2958c6f1c8
Mayank Choudhary <choudharymayank006@gmail.com>
2026-05-01 16:13:08 +0530

    Express server setup

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+const PORT = process.env.PORT ?? 8000
+
+app.get('/', (req, res) => {
+    return res.json({status: 'Server is up and running...'})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+})
+
